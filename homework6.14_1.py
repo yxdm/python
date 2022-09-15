2945dd28c312bc428c0eeeb3361a890bcec0906d
Liuyue <113188092+yxdm@users.noreply.github.com>
2022-09-15 17:49:08 +0800

    Update homework6.14_1.py
    2020144118 刘月 第二次作业

--- homework6.14_1.py
+++ homework6.14_1.py
@@ -1,6 +1,6 @@
 #Exercise: Take the following Python code that stores a string:‘str = 'X-DSPAM-Confidence:0.8475'
 #Use find and string slicing to extract the portion of the string after the colon character
-#and then use the float function to convert the extracted string into a floating point number.
+#and then use the float function to convert the extracted string into a floating point number
 
 str = 'X-DSPAM-Confidence:0.8475'
 
