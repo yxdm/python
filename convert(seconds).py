35da7788d7c0d3c0803535cebcf12af9b51a68ae
Liuyue <113188092+yxdm@users.noreply.github.com>
2022-09-22 17:27:03 +0800

    Update convert(seconds).py

--- convert(seconds).py
+++ convert(seconds).py
@@ -1,3 +1,6 @@
+#接收一个表示纪元秒数的正整数seconds，也就是从1970年1月1日8时0分0秒到目前为止经过的秒数
+#要求返回纪元秒数seconds对应的日期时间字符串。
+#例如：main（1601901810）返回‘2020-10-05_20：43：30’日期和时间之间有一个下画线
 from time import strftime,localtime
 
 def convert(seconds):
